048cefebe054a9cd579665932be95a8e97818c8b
村山正俊 <masatoshi0058@gmail.com>
2023-11-29 00:01:46 +0900

    AC

---
+++ bronze5/26209.py
@@ -0,0 +1,10 @@
+a, b, c, d, e, f, g, h = map(int, input().split())
+
+if a == 9 or b == 9 or c == 9 or d == 9 or e == 9 or f == 9 or g == 9 or h == 9:
+    print("F")
+else:
+    print("S")
+
+# N = map(int, input().split())
+
+# print('F' if 9 in N else 'S')
