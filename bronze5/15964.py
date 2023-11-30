a96d982b20bf59b4f31c1b9edf7cc34817cbf4d3
村山正俊 <masatoshi0058@gmail.com>
2023-11-30 10:26:03 +0900

    AC

---
+++ bronze5/15964.py
@@ -0,0 +1,8 @@
+A, B = map(int, input().split())
+
+N = (A+B)*(A-B)
+
+print(N)
+
+# A, B = map(int, input().split())
+# print((A+B)*(A-B))
--- bronze5/16170.py
+++ bronze5/16170.py
@@ -2,3 +2,10 @@ day = ['2018', '09', '29']
 
 for d in day:
     print(d)
+
+#     import datetime
+# now = datetime.datetime.now() + datetime.timedelta(hours=9) #한국시간으로 나오니 수정해준다
+
+# print(now.year)
+# print(now.month)
+# print(now.day)
